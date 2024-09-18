728d0e17688d7833c522a2e7402cad9c363a417a
Eduard Babetskyi <edward.babetskyi@gmail.com>
2024-09-18 08:20:59 +0300

    updated products

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	$('.shop_button').click(function () {
 		$('.shop_button #menu').toggleClass('open');
+		$('.shop_button').toggleClass('open');
+		$('.menu_list').toggleClass('open');
 	});
 
 	$('.dropdown_list ul li').click(function () {
