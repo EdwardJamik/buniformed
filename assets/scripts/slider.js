d08dc202c0e8366afbcceb7920ce7a04de3c3648
Eduard Babetskyi <edward.babetskyi@gmail.com>
2024-09-24 21:37:29 +0300

    updated products

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -44,4 +44,50 @@ $(document).ready(function () {
 			},
 		],
 	});
+
+	$('.testimonials .slider .slider_list').slick({
+		infinite: false,
+		// autoplay: true,
+		// autoplaySpeed: 1500,
+		prevArrow:
+			"<button type='button' class='slide-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+		nextArrow:
+			"<button type='button' class='slide-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+		customPaging: function (slider, i) {
+			return '<div class="slider_dots"></div>';
+		},
+		speed: 1000,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		arrows: true,
+		dots: true,
+		pauseOnHover: false,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					variableWidth: true,
+				},
+			},
+			{
+				breakpoint: 860,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					variableWidth: true,
+				},
+			},
+			{
+				breakpoint: 520,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					variableWidth: true,
+				},
+			},
+		],
+	});
 });
